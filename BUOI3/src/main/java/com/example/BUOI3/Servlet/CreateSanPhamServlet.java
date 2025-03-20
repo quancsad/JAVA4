fba5b78f2b796b57cae1a3a08b066817f880bc9a
quancsad <quanvhph47140@fpt.edu.vn>
2025-03-20 15:07:30 +0700

    a

---
+++ BUOI3/src/main/java/com/example/BUOI3/Servlet/CreateSanPhamServlet.java
@@ -0,0 +1,22 @@
+package com.example.BUOI3.Servlet;
+
+import com.example.BUOI3.Repo.SanPhamRepository;
+import com.example.BUOI3.model.SanPham;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/create-san-pham")
+public class CreateSanPhamServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SanPhamRepository sanPhamRepository = new SanPhamRepository();
+        SanPham sanPham = new SanPham();
+        sanPham.setName("ahihi");
+        Object object = sanPhamRepository.create(sanPham);
+        resp.getWriter().println(object);
+    }
+}
---
+++ BUOI3/src/main/java/com/example/BUOI3/Servlet/ViewSanPhamServlet.java
@@ -0,0 +1,32 @@
+package com.example.BUOI3.Servlet;
+
+
+import com.example.BUOI3.Repo.SanPhamRepository;
+import com.example.BUOI3.model.SanPham;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+@WebServlet("/view-san-pham")
+public class ViewSanPhamServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Danh sachs san pham</h1>");
+        SanPhamRepository repository = new SanPhamRepository();
+        List<SanPham> sanPhams = repository.getAll();
+        if (sanPhams.isEmpty()){
+            resp.getWriter().println("<h1>Danh sachs rong</h1>");
+        }else {
+            resp.getWriter().println("<ul>");
+            for (SanPham sp : sanPhams) {
+                resp.getWriter().println("<li>" + sp.getId() + " " + sp.getName() + "</li>");
+            }
+            resp.getWriter().println("</ul>");
+        }
+    }
+}
